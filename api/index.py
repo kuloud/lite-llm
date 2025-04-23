96def050a5e769bb4b48993429af665555eb216f
kuloud <kuloud@outlook.com>
2025-04-23 23:59:10 +0800

    revert: delete dia (not enough to run)

--- api/index.py
+++ api/index.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from app.routers.github_models import router as github_models_router
-from app.routers.dia_router import router as dia_router
 
 app = FastAPI()
 
-app.include_router(github_models_router)
-app.include_router(dia_router)
+
+@app.get("/", include_in_schema=False)
+async def root():
+    return {"message": "FastAPI Service is running"}
+
+
+app.include_router(github_models_router, prefix="/api", tags=["Models"])
